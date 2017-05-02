e2959441af11c36af9d75c8d8c97dfe56a39849c
Thomas <thomas.kjellberg@gmail.com>
2017-05-02 15:01:59 +0200

    new functions added: pasue, isSpeaking, isPaused, hasUtterancePending

--- src/core.js
+++ src/core.js
@@ -1,13 +1,17 @@
 //core
-const msg = new SpeechSynthesisUtterance()
+export const msg = new SpeechSynthesisUtterance()
 
-export const speak = (text, { lang, voice, volume, rate }) => {
+export const speak = (text, config = {}) => {
     msg.text   = text
-    msg.lang   = lang || 'sv-SE'
-    msg.volume = volume || 1
-    msg.rate   = rate || 1
+    msg.lang   = config.lang || 'sv-SE'
+    msg.volume = config.volume || 1
+    msg.rate   = config.rate || 1
     
     speechSynthesis.speak(msg)
+}
+
+export const pause = () => {
+    speechSynthesis.pause()
 }
 
 export const windowHasSpeechSynthesis = () => {
@@ -16,7 +20,11 @@ export const windowHasSpeechSynthesis = () => {
     } else {
         return false
     }
-} 
+}
+
+export const isSpeaking = () => (speechSynthesis.pending)
+export const isPaused = () => (speechSynthesis.paused)
+export const hasUtterancesPending = () => (speechSynthesis.pending) 
 
 
 
