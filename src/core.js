6cc18567147f1f13ba416e6efa679483335a677d
Thomas <thomas.kjellberg@gmail.com>
2017-05-02 13:05:18 +0200

    core and component basic structure

--- src/core.js
+++ src/core.js
@@ -1,1 +1,23 @@
 //core
+const msg = new SpeechSynthesisUtterance()
+
+export const speak = (text, { lang, voice, volume, rate }) => {
+    msg.text   = text
+    msg.lang   = lang || 'sv-SE'
+    msg.volume = volume || 1
+    msg.rate   = rate || 1
+    
+    speechSynthesis.speak(msg)
+}
+
+export const windowHasSpeechSynthesis = () => {
+    if ('speechSynthesis' in window) {
+        return true
+    } else {
+        return false
+    }
+} 
+
+
+
+
--- src/ports/react.js
+++ src/ports/react.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { speak, windowHasSpeechSynthesis } from '../core'
 
-export default function Button(WrappedComponent, config = {}) {
+export default function Button(text, config = {}) {
   return class extends Component {
-    render(){
-      return (<p>test</p>)
+    constructor(props) {
+      super(props)
+      this.state = {
+        windowHasSpeechSynthesis: windowHasSpeechSynthesis()
+      }
+    }
+
+    render() {
+      return (
+        <div>
+          <div>
+            <button 
+              onClick={() => speak(text, config)}
+            >
+              Lyssna
+            </button>
+            {this.state.windowHasSpeechSynthesis && <p>Din webbläsare stödjer inte text till tal</p>}
+          </div>
+          {this.props.children}
+        </div>
+      )
     }
   }
 }
