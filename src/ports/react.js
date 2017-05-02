36c880600db1727b9a92b0b5ffd327726a855667
Thomas <thomas.kjellberg@gmail.com>
2017-05-02 15:02:39 +0200

    states added and 'speak' function

--- src/ports/react.js
+++ src/ports/react.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react'
-import { speak, windowHasSpeechSynthesis } from '../core'
+import { speak, windowHasSpeechSynthesis, isPaused, isSpeaking, hasUtterancesPending } from '../core'
 
 export default class extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      windowHasSpeechSynthesis: windowHasSpeechSynthesis()
+      windowHasSpeechSynthesis: windowHasSpeechSynthesis(),
+      speaking: isSpeaking(),
+      paused: isPaused(),
+      pending: hasUtterancesPending(),
     }
   }
 
+  speak() {
+    const { text, config } = this.props
+
+    this.setState({ speaking: isSpeaking })
+    speak(text, config)
+  }
+
   render() {
-    const { text } = this.props
+    const { speaking, paused, pending } = this.state
+    
     return (
       <div>
         <div>
-          <button 
-            onClick={() => speak(text, config)}
-          >
-            Lyssna
-          </button>
+          {!speaking && <button onClick={() => this.speak()}>Lyssna</button>}
+          {speaking && <button onClick={() => pause()}>Pause</button>}
           {!this.state.windowHasSpeechSynthesis && <p style={{ color: 'red' }}>Din webbläsare stödjer inte text till tal</p>}
         </div>
         {this.props.children}
